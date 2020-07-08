de050a01d167635f7f11c19521d24bd69c6da9c6
Monica <mnoritake@gmail.com>
2020-07-08 16:11:49 -0300

    criacao do arquvo variaveis.java

---
+++ Variaveis.java
@@ -0,0 +1,22 @@
+class Variaveis{
+public static void main(String args[]){
+// para armazenar um nome
+String nome = "Monica";
+String sobrenome = "Noritake";
+// para armazenar o peso de uma pessoa
+float peso = 100;
+// para armazenar a altura de uma pessoa
+float altura = 2.00f;
+// idade
+
+// variável para armazenar o resultado do IMC
+float imc = peso / (altura * altura);
+
+int idade = 90;
+System.out.println("Nome...: " + nome + " " + sobrenome);
+System.out.println("Peso...: " + peso);
+System.out.println("Altura.: " + altura);
+System.out.println("Idade..: " + idade);
+System.out.println("Seu IMC: " + imc);
+}
+}
